a8dcd6d278d606a903eed06291fda0314b15c023
k33ngTomide <Msodiqakintomide@gmail.com>
2023-10-19 20:03:11 +0100

    function in a function

--- minute_25/hash_delete.py
+++ minute_25/hash_delete.py
@@ -2,7 +2,7 @@ def is_equal_after_backspace(first_word: str, second_word: str):
     def impl_backspace(word) -> bool:
         for index in range(len(word)):
             if word[index] == '#':
-                new_word = (first_word
+                new_word = (word
                             .replace(word[index], "")
                             .replace(word[index - 1], ""))
         return new_word
